0fbbdd5f1ae28f80a47d8f655c538d8fe23a22ea
Muhammad_Sulaiman <salluansari777890@gmail.com>
2023-08-24 17:42:27 +0500

    3 Pages are complete

--- app/(components)/Heading/Heading.js
+++ app/(components)/Heading/Heading.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import SingleButton from '../Button/SingleButton'
 
 export default function Heading(props) {
@@ -6,7 +5,7 @@ export default function Heading(props) {
     <div className="container">
       <div className="row">
             <div className="col-12">
-                <h3  className="text-primary">{props.h3}</h3>
+                <h1  className="text-primary fw-bold">{props.h1}</h1>
               <h1 className='fw-bold'>{props.title}</h1>
               <p className='my-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, esse? Minima eius a eaque quam vitae temporibus voluptate, at ratione, nesciunt tenetur aliquam laborum ipsum rem labore.</p>
               <SingleButton button={props.button}/>
--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Header from '../(components)/PagesHeader/Header'
+import Heading from '../(components)/Heading/Heading'
+import Image from 'next/image'
+import gruop from "../assets/images/about.png"
 
 export default function About() {
   return (
     <div>
-      <Header title="About"/>
+      <Header title="About" />
+      <div className="container thisClass">
+        <div className="row">
+          <div className="col-6">
+            <Heading h1="We 'er The World's One Of The Best Company" button="Read More" />
+          </div>
+          <div className="col-6">
+            <Image height={350} src={gruop} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
