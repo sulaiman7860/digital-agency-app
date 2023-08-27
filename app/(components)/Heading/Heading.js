d2fc0592d590862e826828cd738b7dd92c04fbcb
Muhammad_Sulaiman <salluansari777890@gmail.com>
2023-08-28 03:50:56 +0500

    4 pages are complete

--- app/(components)/Heading/Heading.js
+++ app/(components)/Heading/Heading.js
@@ -5,7 +5,7 @@ export default function Heading(props) {
     <div className="container">
       <div className="row">
             <div className="col-12">
-                <h1  className="text-primary fw-bold">{props.h1}</h1>
+                <h1  className="fw-bold" style={{color:"#06d6a0"}}>{props.h1}</h1>
               <h1 className='fw-bold'>{props.title}</h1>
               <p className='my-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, esse? Minima eius a eaque quam vitae temporibus voluptate, at ratione, nesciunt tenetur aliquam laborum ipsum rem labore.</p>
               <SingleButton button={props.button}/>
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import Header from '../(components)/PagesHeader/Header'
+import Card from '../(components)/Card/Card'
+import Images from '../(components)/Image/Images'
 
 export default function Contact() {
   return (
     <div>
-      <Header title="Portfolio"/>
+      <Header title="Portfolio" />
+      <div className="container thisClass">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1 className='fw-bold' style={{ color: "#06d6a0" }}>Course We Can Offer</h1>
+            <p className='mt-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="col-4">
+            <Card title="Undergraduate Programs"/>
+          </div>
+          <div className="col-4">
+            <Card title="Graduate Programs"/>
+          </div>
+          <div className="col-4">
+            <Card title="Adult Learning"/> 
+          </div>
+        </div>
+      </div>
+      <div className="container thisClass">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1 className='fw-bold' style={{ color: "#06d6a0" }}>Course We Can Offer</h1>
+            <p className='my-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <Images title1="Web Development" title2="Artificial Intelligent" title3="Data Science" 
+          prag1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, vel magni rerum."
+          prag2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, vel magni rerum."
+          prag3="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, vel magni rerum."/>
+        </div>
+      </div>
     </div>
   )
 }
